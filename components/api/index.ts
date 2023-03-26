f8a1f302f16587f3b4fa9aca957f8fd82f6a5c42
Guilherme Ramos <guiramos@gmail.com>
2023-03-26 14:58:28 -0400

    Adding methods to interact with conversations

--- components/api/index.ts
+++ components/api/index.ts
@@ -1,7 +1,8 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
-import { env } from '@/components/Providers/Environment'
+import axios, {AxiosRequestConfig, AxiosResponse} from 'axios'
+import {env} from '@/components/Providers/Environment'
 import axiosRetry from 'axios-retry'
-import { ChatMessageEntity } from "@/types/custom";
+import {ChatMessageEntity} from "@/types/custom";
+import {Conversation} from "@/types";
 
 export type ChatHttpConfig = {
   url: string
@@ -13,6 +14,7 @@ type HTTP = {
   get<T = unknown, R = AxiosResponse<T>>(config: ChatHttpConfig): Promise<R>
   post<T = unknown, R = AxiosResponse<T>>(config: ChatHttpConfig, data?: unknown): Promise<R>
   put<T = unknown, R = AxiosResponse<T>>(config: ChatHttpConfig, data?: unknown): Promise<R>
+  delete<T = unknown, R = AxiosResponse<T>>(config: ChatHttpConfig, data?: unknown): Promise<R>
 }
 
 
@@ -21,25 +23,7 @@ export const requestInterceptor = (value: AxiosRequestConfig) => {
   return value
 }
 axios.interceptors.request.use(requestInterceptor)
-axiosRetry(axios, { retries: 0 })
-
-export const httpClient: HTTP = {
-  get: (config) => {
-    return axios.request(config)
-  },
-  post: (config, payload) => {
-    return axios.request({
-      data: payload,
-      ...config
-    })
-  },
-  put: (config, payload) => {
-    return axios.request({
-      data: payload,
-      ...config
-    })
-  }
-}
+axiosRetry(axios, {retries: 0})
 
 export const chatClient: HTTP = {
   get: (config) => {
@@ -67,12 +51,63 @@ export const chatClient: HTTP = {
       data: payload,
       ...config
     })
+  },
+  delete: (config, payload) => {
+    return axios.request({
+      method: 'delete',
+      responseType: 'json',
+      baseURL: env('butleyApiHost'),
+      data: payload,
+      ...config
+    })
   }
 }
 
+// Create a new conversation
+export const createConversation = (conversation: Conversation) =>
+    chatClient.post<Conversation>(
+        {
+          url: `/conversation`
+        },
+        conversation
+    );
+
+// Get conversations by userId
+export const getConversations = (userId: string) =>
+    chatClient.get<Conversation[]>(
+        {
+          url: `/conversation/${userId}`
+        }
+    );
+
+export const deleteConversation = (conversationId: string) =>
+    chatClient.delete(
+        {
+          url: `/conversation/${conversationId}`
+        }
+    );
+
+
+export const createMessage = (chatMessageEntity: ChatMessageEntity) =>
+    chatClient.post(
+        {
+          url: `/chat`
+        },
+        chatMessageEntity
+    );
+
+// Get messages by conversationId and userId
+export const getMessagesByConversationId = (conversationId: string, userId: string) =>
+    chatClient.get<ChatMessageEntity[]>(
+        {
+          url: `/chat/messages/conversation/${conversationId}/${userId}`
+        }
+    );
+
+
 export const getMessages = (userId: string) =>
-  chatClient.get<ChatMessageEntity[]>(
-      {
-        url: `/chat/messages/last/${userId}/0`
-      }
-  )
+    chatClient.get<ChatMessageEntity[]>(
+        {
+          url: `/chat/messages/last/${userId}/0`
+        }
+    )
